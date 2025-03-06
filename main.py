ce25a2486c33578557e78d438c430958ed4d8a00
jerryzls <167752658+jerryzls@users.noreply.github.com>
2025-03-06 17:46:07 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# 导入PyOfficeRobot模块，pip install PyOfficeRobot
+import PyOfficeRobot
+
+PyOfficeRobot.chat.chat_by_deepseek(
+                who='微信昵称',
+                api_key="sk-12ce3f4dfe56789dac77213f83766263", url="https://api.deepseek.com/chat/completions")
+
+# # PyOfficeRobot.chat.chat_robot(who='微信昵称')
